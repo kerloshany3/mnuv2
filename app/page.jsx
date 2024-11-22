89c35159b96097bf192e04840754caeb13ae7a7b
kerloshany3 <kerlostablet@gmail.com>
2024-11-22 21:11:34 +0200

    the last

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Physics from "./components/Physics";
 import Section from "./components/Section";
-
+import Image from "next/image";
 
 
 export default function Home() {
@@ -9,10 +9,11 @@ export default function Home() {
 
       <div className=" shadow-sm shadow-slate-400/40 bg-slate-700 border-slate-600 border-4 m-5 rounded-xl pb-5">
         <div className="  m-auto flex justify-center p-10">
-          <h2 className=" font-bold text-7xl text-slate-400 shadow-2xl shadow-slate-600 border-slate-400 border-4 p-2 rounded-xl">MNU</h2>
+          <div className=" bg-slate-400 shadow-2xl shadow-slate-600 border-slate-400 border-4 p-2 rounded-xl justify-center justify-items-center place-items-center flex">
+            <h2 className=" font-bold text-7xl text-slate-700">MNU</h2>
+            <Image src="logominia.svg" width={80} height={80} alt="logo"></Image>
 
-
-
+          </div>
         </div>
 
         <div className="  flex font-arabicUI text-slate-400 border-4 p-4 m-auto justify-center w-11/12 rounded-xl shadow-xl bg-slate-800/60 shadow-slate-800/70 border-slate-400">
