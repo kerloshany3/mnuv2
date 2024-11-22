26401c28874615a6758786b852829f821649ca3d
kerloshany3 <kerlostablet@gmail.com>
2024-11-22 17:47:10 +0200

    shit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+const ArabicUI = localFont({ src: './fonts/ArabicUIDisplayBlack.ttf' })
+import { Abril_Fatface } from 'next/font/google';
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,10 +23,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${ArabicUI.className}  bg-slate-800`}>
         {children}
+
+        <Link href="/">
+        <button className=" active:bg-slate-500 active:ring-4 active:ring-slate-800/20 active:duration-500 active:text-slate-800 font-abril bg-slate-800 m-auto flex justify-center p-2 border-2 border-slate-400 w-fit  rounded-lg mb-6 text-slate-400">created by KerlosDev</button>
+        </Link>
+
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,11 +3,14 @@ import Section from "./components/Section";
  */
 export default function Home() {
   return (
-    <div className=" ">
+    <div className=" font-bungee ">
 
       <div className=" shadow-sm shadow-slate-400/40 bg-slate-700 border-slate-600 border-4 m-5 rounded-xl pb-5">
         <div className="  m-auto flex justify-center p-10">
           <h2 className=" font-bold text-7xl text-slate-400 shadow-2xl shadow-slate-600 border-slate-400 border-4 p-2 rounded-xl">MNU</h2>
+          <h3 className=" font-bungee">fuck 5565</h3>
+
+          
         </div>
 
         <div className="  flex font-arabicUI text-slate-400 border-4 p-4 m-auto justify-center w-11/12 rounded-xl shadow-xl bg-slate-800/60 shadow-slate-800/70 border-slate-400">
@@ -15,6 +18,7 @@ export default function Home() {
             <h2 className=" w-3/4 text-center border-2 border-slate-400 p-2 rounded-lg m-auto flex justify-center text-2xl bgsl "> اكتب رقم السكشن  الاساسي بتاعك</h2>
 {/*             <Section></Section>
  */}          </div>
+          
         </div>
 
         <div className=" mt-10 flex font-arabicUI bg-slate-800/60 text-slate-400 border-4 p-4 m-auto justify-center w-11/12 rounded-xl shadow-xl shadow-slate-800/70 border-slate-400">
