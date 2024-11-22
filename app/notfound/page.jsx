e8258eceea5ab5517ed0b51cea0b6bb6da8f9223
kerloshany3 <kerlostablet@gmail.com>
2024-11-22 19:00:00 +0200

    add notfound

---
+++ app/notfound/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const notfound = () => {
+  return (
+      <div>
+          <h2 className=' font-arabicUI text-5xl leading-relaxed  text-slate-400 border-4 border-slate-400 p-3  m-4 rounded-2xl text-center mx-4 flex justify-center place-items-center'>اي يهندسة مفيش سكشن بالرقم ده </h2>
+    </div>
+  )
+}
+
+export default notfound
