ee9fc30c71bdab5581554e7cb8766d22cd38a5d7
kerloshany3 <kerlostablet@gmail.com>
2024-11-22 19:29:30 +0200

    removerd

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,24 +1,13 @@
-'use client';
-import { Suspense } from 'react';
+// app/result/page.js
 
-import React from 'react';
-import { useSearchParams } from 'next/navigation';
-
-const Result = () => {
-    const searchParams = useSearchParams();
-    const numSec = searchParams.get('numSec'); // Retrieve the "numSec" query parameter
+export default function Result({ searchParams }) {
+    const numSec = searchParams?.numSec || 'No value provided';
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-
-            <div className="m-auto justify-center place-items-center flex flex-col">
-                <h1 className="font-bungee text-7xl p-4 mt-4 text-slate-900 bg-slate-500 rounded-xl w-6/12 text-center">
-                    Value from URL: {numSec}
-                </h1>
-            </div>
-        </Suspense>
-
+        <div className="m-auto justify-center place-items-center flex flex-col">
+            <h1 className="font-bungee text-7xl p-4 mt-4 text-slate-900 bg-slate-500 rounded-xl w-6/12 text-center">
+                Value from URL: {numSec}
+            </h1>
+        </div>
     );
-};
-
-export default Result;
+}
