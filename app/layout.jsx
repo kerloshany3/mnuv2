9c2c4004ebe130f4a08f543931bd970b44b7bdcf
kerloshany3 <kerlostablet@gmail.com>
2024-11-22 20:42:19 +0200

    fix logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,11 +22,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    
+      
+
       <body className={`${ArabicUI.className}  bg-slate-800`}>
         {children}
 
         <Link href="/">
-        <button className=" active:bg-slate-500 active:ring-4 active:ring-slate-800/20 active:duration-500 active:text-slate-800 font-abril bg-slate-800 m-auto flex justify-center p-2 border-2 border-slate-400 w-fit  rounded-lg mb-6 text-slate-400">Created By KerlosDev</button>
+          <button className=" active:bg-slate-500 active:ring-4 active:ring-slate-800/20 active:duration-500 active:text-slate-800 font-abril bg-slate-800 m-auto flex justify-center p-2 border-2 border-slate-400 w-fit  rounded-lg mb-6 text-slate-400">Created By KerlosDev</button>
         </Link>
 
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 import Physics from "./components/Physics";
 import Section from "./components/Section";
- 
 
-import { dataofcollegearba } from "./data";
+
+
 export default function Home() {
   return (
     <div className=" font-bungee lg:w-2/6 justify-center m-auto ">
@@ -18,8 +18,8 @@ export default function Home() {
         <div className="  flex font-arabicUI text-slate-400 border-4 p-4 m-auto justify-center w-11/12 rounded-xl shadow-xl bg-slate-800/60 shadow-slate-800/70 border-slate-400">
           <div className="place-items-center">
             <h2 className=" w-3/4 text-center border-2 border-slate-400 p-2 rounded-lg m-auto flex justify-center text-2xl bgsl "> اكتب رقم السكشن  الاساسي بتاعك</h2>
-             <Section></Section>
-  </div>
+            <Section></Section>
+          </div>
 
         </div>
 
