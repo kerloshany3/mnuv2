6cedcc0500b368d215aa00cebab4fe00e7af0c45
kerloshany3 <kerlostablet@gmail.com>
2024-11-22 19:23:58 +0200

    upd2

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -8,11 +8,15 @@ const Result = () => {
     const numSec = searchParams.get('numSec'); // Retrieve the "numSec" query parameter
 
     return (
-        <div className="m-auto justify-center place-items-center flex flex-col">
-            <h1 className="font-bungee text-7xl p-4 mt-4 text-slate-900 bg-slate-500 rounded-xl w-6/12 text-center">
-                Value from URL: {numSec}
-            </h1>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+
+            <div className="m-auto justify-center place-items-center flex flex-col">
+                <h1 className="font-bungee text-7xl p-4 mt-4 text-slate-900 bg-slate-500 rounded-xl w-6/12 text-center">
+                    Value from URL: {numSec}
+                </h1>
+            </div>
+        </Suspense>
+
     );
 };
 
